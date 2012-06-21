7851355fb63fb8bec79cf7f4af77eb8901c0d565
Eugene Ma <edma2@san.rr.com>
2012-06-21 03:39:15 -0700

    assertion strings

--- geohash.scala
+++ geohash.scala
@@ -2,7 +2,7 @@
  * max is the maximum absolute value that v may take.
  * abs(v) should not be greater than max. */
 class FloatEncoder(v: Double, max: Double)  {
-    assert(v.abs <= max)
+    assert(v.abs <= max, "Absolute value greater than max")
 
     def encode: Stream[Int] = encode(max, 0)
 
@@ -41,8 +41,8 @@ object Base32 {
     val POWERS = Array(16,8,4,2,1)
 
     def encode(bits: Stream[Int]): Stream[Char] = {
-        val ch = BASE32((bits zip POWERS).collect{case(a,b) => a*b}.sum) 
-        ch #:: encode(bits drop POWERS.size)
+        val x = (bits zip POWERS).collect{case(a,b) => a*b}.sum
+        BASE32(x) #:: encode(bits drop POWERS.size)
     }
 }
 
@@ -50,8 +50,9 @@ object Base32 {
  * Produce Coordinate bits by interleaving latitude and longitude bits
  * Base32 encode the bit stream. */
 class Geohash(lat: Double, lon: Double) {
-    assert(lat.abs <= 90)
-    assert(lon.abs <= 180)
+    assert(lat.abs <= 90, "Latitude not between -90 and 90")
+    assert(lon.abs <= 180, "Longitude not between -180 and 180")
+
     val hash = Base32.encode(new CoordEncoder(lat, lon).encode)
 
     val DEFAULT_PRECISION = 12
@@ -59,6 +60,9 @@ class Geohash(lat: Double, lon: Double) {
     def toString(len: Int): String = hash.take(len).toList.mkString
 
     override def toString = toString(DEFAULT_PRECISION)
+
+    val neighbors: Char => String = {
+    }
 }
 
 object Test {
